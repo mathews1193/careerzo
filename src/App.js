fed07adae6d6154d3cf6bd438b2506110051ca93
mathews1193 <mathews1193@yahoo.com>
2021-08-23 10:20:45 -0500

    added main pages to navigate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import Home from './containers/Home';
+import Login from './containers/Login';
+import SignUp from './containers/SignUp';
+import Pathway from './containers/Pathway';
+import Profile from './containers/Profile';
+import Dashboard from './containers/Dashboard';
+import Messager from './containers/Messager';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CareerZo</h1>
+      <Router>
+        <div className='container'>
+          <Route exact path="/" component={Home}/>
+          <Route path="/signup"component={SignUp} />
+          <Route path="/login"component={Login} />
+          <Route path="/career-pathway"component={Pathway} />
+          <Route path="/profile"component={Profile} />
+          <Route path="/dashboard"component={Dashboard} />
+          <Route path="/messager"component={Messager} />
+        </div>
+      </Router>
     </div>
   );
 }
-
 export default App;
