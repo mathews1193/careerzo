65e2909b71859c5c1a08eb0577a5e3e5cd03373c
mathews1193 <mathews1193@yahoo.com>
2021-08-26 10:54:44 -0500

    updated style and layout of career pathway page

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Pathway from './containers/Pathway';
 import Profile from './containers/Profile';
 import Dashboard from './containers/Dashboard';
 import Messager from './containers/Messager';
-import './App.css';
 import Navbar from './components/NavBar/Navbar';
 import Footer from './components/Footer/Footer';
+import Certification from './containers/Certification';
+import Positions from './containers/Positions';
+import './App.css';
 
 function App() {
   return (
@@ -24,6 +26,8 @@ function App() {
           <Route path="/profile"component={Profile} />
           <Route path="/dashboard"component={Dashboard} />
           <Route path="/messager"component={Messager} />
+          <Route path="/certification"component={Certification} />
+          <Route path="/past-position"component={Positions} />
         </div>
         <Footer />
       </Router>
--- src/components/CareerPathway/Pathway.js
+++ src/components/CareerPathway/Pathway.js
@@ -5,7 +5,7 @@ import './Pathway.css';
 
 function Pathway() {
     var position = "Foreman";
-    var level = "Senior";
+    var level = "Mid";
     var name = "Steve Rogers";
     const img = [
         'https://images.pexels.com/photos/8005397/pexels-photo-8005397.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
@@ -13,43 +13,46 @@ function Pathway() {
 
     return (
         <div>
-            <h1 className="head">Career Pathway: {name}</h1>
-            <img src={img[0]} alt="Logo" width="25%" height="25%"/>
-            <div className="path-infor">
-                <h2 className="head">Current Level: {level} Level {position}</h2>
-                <h2 className="head">Promotion Requirements: Not Meet</h2>
-            </div>
+            <div className="background5">
+                <h1 className="title">Career Pathway: {name}</h1>
+                <img src={img[0]} alt="Logo" width="23%" height="30%"/>
+                <div className="path-infor">
+                    <h2 className="head">Current Level: {level} Level {position}</h2>
+                    <h2 className="head">Promotion Requirements: Not Meet</h2>
+                </div>
 
-            <ProgressBar 
-            completed={60}
-            bgColor={'#eafa04e3'}
-            labelColor={'black'}
-            width={'60%'}
-            margin={'0 auto'}
-             />
+                <ProgressBar 
+                completed={45}
+                bgColor={'#eafa04e3'}
+                labelColor={'black'}
+                height={'110%'}
+                width={'75%'}
+                margin={'0 auto'}
+                />
 
-             { level === "Entry" ? (
-             <div>
-                <h2 className="title">Entry Level {position}</h2> 
-                <h2 className="title">Mid Level {position}</h2>
-                <h2 className="title">Next Level Promotion</h2>
-                <h2 className="title">Senior Level {position}</h2>
-                <h2 className="title">Reach Mid Level Position First!</h2> 
+                { level === "Entry" ? (
+                <div className="level">
+                    <h2 className="head">Entry Level {position}</h2> 
+                    <h2 className="head">Mid Level {position}</h2>
+                    <h2 className="head">Next Level Promotion</h2>
+                    <h2 className="head">Senior Level {position}</h2>
+                    <p className="head">Reach Mid Level Position First!</p> 
+                </div>
+                ) : ( level === "Mid") ? (
+                <div className="level">
+                    <h2 className="head"> <AssignmentTurnedInIcon /> Entry Level {position}</h2> 
+                    <h2 className="head">Mid Level {position}</h2>
+                    <h2 className="head">Senior Level {position}</h2>
+                    <p className="head">Next Level Promotion!</p>
+                </div>
+                ) : (
+                <div className="level">
+                    <h2 className="head"><AssignmentTurnedInIcon /> Entry Level {position}</h2> 
+                    <h2 className="head"><AssignmentTurnedInIcon /> Mid Level {position}</h2>
+                    <h2 className="head">Senior Level {position}</h2>
+                </div>
+                )}
             </div>
-            ) : ( level === "Mid") ? (
-            <div>
-                <h2 className="title"> <AssignmentTurnedInIcon /> Entry Level {position}</h2> 
-                <h2 className="title">Mid Level {position}</h2>
-                <h2 className="title">Senior Level {position}</h2>
-                <h2 className="title">Next Level Promotion!</h2>
-            </div>
-            ) : (
-                <div>
-                <h2 className="title"><AssignmentTurnedInIcon /> Entry Level {position}</h2> 
-                <h2 className="title"><AssignmentTurnedInIcon /> Mid Level {position}</h2>
-                <h2 className="title">Senior Level {position}</h2>
-            </div>
-            )}
         </div>
     )
 }
