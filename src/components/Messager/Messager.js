87abb01ca33ec74a722e6371d5b2e495f55c8e8b
mathews1193 <mathews1193@yahoo.com>
2021-08-27 06:45:01 -0500

    added content and style to message board

--- src/components/Messager/Messager.js
+++ src/components/Messager/Messager.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
+import firebase from '../../Firebase/firebase1';
 import './Messager.css';
 
 function Messager() {
+    const [message, setMessage] = useState("");
+    const [to, setTo] = useState("");
+    const [from, setFrom] = useState("");
+
+    const createMsg = () => {
+        firebase
+        .firestore()
+        .collection("message")
+        .add({
+        msg: message,
+        to: to,
+        from:from,
+        })
+        .then(ref => {
+        console.log("Added document with ID: ", ref.id)
+        })
+    };
     return (
-        <div>
-            <h1>Messager</h1>
+        <div className="background6">
+            <h1 className="title">Message Board</h1>
+            <input
+            className="form1"
+            required
+            value={from}
+            type="text"
+            placeholder="From"
+            onChange={(e) => {
+            setFrom(e.target.value);
+            }}
+            />  
+            <input
+            className="form1"
+            required
+            value={to}
+            type="text"
+            placeholder="To"
+            onChange={(e) => {
+            setTo(e.target.value);
+            }}
+            />  
+            <input
+            className="form1"
+            required
+            value={message}
+            type="text"
+            placeholder="Message"
+            onChange={(e) => {
+            setMessage(e.target.value);
+            }}
+            />  
+
         </div>
     )
 }
